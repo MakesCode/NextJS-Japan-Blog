14c33254f3a26b15caa056cd35ff86204726b6bd
MakesCode <makescodeur@gmail.com>
2022-05-20 18:25:56 +0200

    ajout du bloc titre page d'accueil

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import styles from '../styles/Home.module.css'
+import style from '../styles/Home.module.css'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main >
-
+      <div className={style.bloc_titre}>
+        <h1 className={style.titre}>10 façons incroyables de voir le japon</h1>
+        <div className={style.circle} />
+      </div>
       blog
       blog
       blog
